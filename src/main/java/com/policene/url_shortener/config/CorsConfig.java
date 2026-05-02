22a61cb70fd10cbc6778a1fe0724724f6ba66308
Giovanni <giovannicpoli@gmail.com>
2026-05-02 20:36:12 -0300

    feat: cors

---
+++ src/main/java/com/policene/url_shortener/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.policene.url_shortener.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${app.front-end-url}")
+    private String frontEndUrl;
+
+    @Bean
+    public CorsFilter corsFilter () {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(frontEndUrl);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+
+    }
+
+
+}
